3078ab8112def0d1d0774d8a2418489524e33ff7
Rich Matthew Arrogante <oznerich@gmail.com>
2025-05-30 12:26:35 +0800

    Update reset-password.js

--- reset-password.js
+++ reset-password.js
@@ -1,9 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabase = createClient(
-  'https://YOUR-PROJECT-ID.supabase.co',
-  'YOUR-ANON-KEY'
-)
+  'https://rdgahcjjbewvyqcfdtih.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZ2FoY2pqYmV3dnlxY2ZkdGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MzI5OTAsImV4cCI6MjA2MzMwODk5MH0.q0LtxZt6-sCWxBKpPnHc6Gn34I11KVJkqvhPHqnEqIU'
+);
 
 async function resetPassword() {
   const password = document.getElementById('new-password').value.trim()
